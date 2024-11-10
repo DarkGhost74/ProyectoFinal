6a46c499d09fde34df16af3049e3b96a100ef7ff
Uziel Omar Flores Torres <uziel.florest@uanl.edu.mx>
2024-11-09 23:00:49 -0600

    Creación de boton rol para los usuarios

--- Scripts/administrar-elementos.js
+++ Scripts/administrar-elementos.js
@@ -10,6 +10,17 @@ $(document).ready(function () {
                     <td><img src="../Imagenes/Prueba/Harry Potter y la piedra filosofal.png" alt="" class="libro"></td>
                     <td>El contenido variará dependiendo de si son los autores, libros o usuarios, así como también la cantidad de columnas</td>
                     <td>Esta tambien</td>
+                    <td>
+                        <div class="dropdown">
+                            <a class="btn btn-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Rol
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Lector</a></li>
+                                <li><a class="dropdown-item" href="#">Administrador</a></li>
+                            </ul>
+                        </div>
+                    </td>
                     <td><button type="button" class="btn btn-dark">Info</button></td>
                     <td><button type="button" class="btn btn-dark">Editar</button></td>
                     <td><button type="button" class="btn btn-dark">Eliminar</button></td>
