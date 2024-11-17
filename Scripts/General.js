f3cee854f75cce5c94d07f54589b743c1a8a7372
Uziel Omar Flores Torres <uziel.florest@uanl.edu.mx>
2024-11-16 21:44:04 -0600

    Creacion de pantalla resultado-busqueda

--- Scripts/General.js
+++ Scripts/General.js
@@ -6,3 +6,8 @@ if (RolId == 1) {
         boton.classList.toggle("admin")
     });
 }
+
+document.querySelector(".btn-outline-success").addEventListener("click", function(event) {
+    event.preventDefault()
+    window.location.href = "resultado-busqueda.html"
+})
