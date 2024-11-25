3e2f35159850befd215ab5872b5ebf2084719be5
Uziel Omar Flores Torres <uziel.florest@uanl.edu.mx>
2024-11-25 14:45:37 -0600

    Añadiendo los enlaces a las pantallas que faltaban

--- Scripts/administrar-generos.js
+++ Scripts/administrar-generos.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
                     <th scope="row">${i + 1}</th>
                     <td>Misterio</td>
                     <td>${i + 13}</td>
-                    <td><button type="button" class="btn btn-dark"><i class="bi bi-info-circle"></i> Info</button></td>
+                    <td><a href="explorar-un-genero.html" type="button" class="btn btn-dark"><i class="bi bi-info-circle"></i> Info</a></td>
                     <td><a href="crear-editar-genero.html" type="button" class="btn btn-dark"><i class="bi bi-pencil"></i> Editar</a></td>
                     <td><a type="button" class="btn btn-dark eliminar-btn" href="#" data-bs-toggle="modal" data-bs-target="#modalEliminar"><i class="bi bi-trash me-1"></i>Eliminar</a></td>
                 </tr>
